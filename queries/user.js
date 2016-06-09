cdffea73c771a921066da5f9a46c3ae6d413960b
Wee Sritippho <Wee Sritippho>
2016-06-09 23:52:37 +0700

    Create custom error when username/password is invalid

--- queries/user.js
+++ queries/user.js
@@ -1,18 +1,25 @@
 var sqlite3 = require('sqlite3');
-
 var db = new sqlite3.Database('storage.db');
 
-exports.login = (username, password, callback) => {
-  db.get("SELECT uid, gid, username, displayName, role FROM users WHERE username=? AND password=?", [username, password], (error, data) => {
-    if (error) {
-      callback(error, null);
+module.exports.login = (username, password, callback) => {
+  db.get("SELECT uid, gid, username, displayName, role FROM users WHERE username=? AND password=?", [username, password], (err, data) => {
+    if (err) {
+      callback(err, null);
       return;
     }
     if (data == null) {
-      error = new Error('InvalidCredentials');
-      callback(error, null);
+      err = new InvalidCredentialsError();
+      callback(err, null);
       return;
     }
     callback(null, data);
   });
 };
+
+function InvalidCredentialsError(message) {
+  this.name = "InvalidCredentialsError";
+  this.message = message || 'Invalid username/password';
+  this.stack = (new Error()).stack;
+}
+InvalidCredentialsError.prototype = Object.create(Error.prototype);
+InvalidCredentialsError.prototype.constructor = InvalidCredentialsError;
