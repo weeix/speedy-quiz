0cbe98c530baff86be975931f3f93fa472ecdd1c
Wee Sritippho <weeaix@gmail.com>
2016-06-20 13:56:27 +0700

    Detect clients IP address and group ID

--- socket.js
+++ socket.js
@@ -4,15 +4,24 @@ module.exports = function (server, jwt, secret) {
     var io = require('socket.io')(server);
 
     io.on('connection', function (socket) {
+        var ipAddr = socket.request.connection.remoteAddress;
         socket.on('join', function (token, gid, callback) {
             jwt.verify(token, secret, function (err, decoded) {
                 if (err) {
                     callback(err);
                     return;
                 }
-                var roomID = gid.toString();
+                if (gid) {
+                    var roomID = gid.toString();
+                } else if (decoded.gid) {
+                    var roomID = decoded.gid.toString();
+                } else {
+                    err = new Error('Group ID undefined');
+                    callback(err);
+                    return;
+                }
                 socket.join(roomID);
-                console.log(decoded.displayName + ' joined room ID: ' + gid);
+                console.log(decoded.displayName + ' (' + ipAddr + ') joined room ID: ' + gid);
                 if (decoded.role == 0) {
                     socket.on('ask', function (qid) {
                         question.ask(qid, function (err, row) {
